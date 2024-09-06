0203bc788ed08b51fb0da933008e7f3d95b69d6a
Huy Dang <ddhuy137@yahoo.com>
2024-09-05 18:06:15 -0700

    added SPDLOG

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ OS  = size
 # Compiling flags & linking flags
 INCLUDE  = -Ilibs
 CXXFLAGS = -Wall -g -O2 -std=c++17
-LDFLAGS  = -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua5.3 libs/rxi/rxi.a 
+LDFLAGS  = -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua5.3
 # Source files
 MODULES = Game Logger
 SRC_DIRS = $(addprefix src/,$(MODULES)) src
--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -1,16 +1,14 @@
 #include "Logger.h"
-extern "C" {
-#include "rxi/log.h"
-}
+#include "spdlog/spdlog.h"
 #include <iostream>
 
 void Logger::Log(const std::string &message)
 {
-    log_info(message.c_str());
+    spdlog::info(message.c_str());
 }
 
 
 void Logger::Err(const std::string &message)
 {
-    log_error(message.c_str());
+    spdlog::error(message.c_str());
 }
